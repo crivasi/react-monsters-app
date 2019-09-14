97a5c580610e15fa978cf2869fe69e29e45861ee
Christian Rivas <christian.x.rivas.-nd@disney.com>
2019-09-14 18:04:40 -0500

    Updates

---
+++ precache-manifest.c5d8d022fc95070f005fcc43017ad645.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695ab140093e5825e4a903f80e0d4ea4",
+    "url": "/react-monsters-app/index.html"
+  },
+  {
+    "revision": "afa709bd93c22335f732",
+    "url": "/react-monsters-app/static/css/main.df9ead4b.chunk.css"
+  },
+  {
+    "revision": "bba759858ed00fd5d4ad",
+    "url": "/react-monsters-app/static/js/2.a10f6eae.chunk.js"
+  },
+  {
+    "revision": "afa709bd93c22335f732",
+    "url": "/react-monsters-app/static/js/main.cf753a87.chunk.js"
+  },
+  {
+    "revision": "101d871941552e462338",
+    "url": "/react-monsters-app/static/js/runtime~main.c545ff31.js"
+  }
+]);
